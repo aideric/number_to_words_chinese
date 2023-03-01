e733de4fde27cb73b0c02e4d3836a2173fcc3478
aideric <kenhoking10@gmail.com>
2023-03-01 11:39:18 +0800

    remove NumberToWordsChineseConstant class

--- lib/number_to_words_chinese.dart
+++ lib/number_to_words_chinese.dart
@@ -11,11 +11,11 @@ class NumberToWordsChinese {
     // }
     final StringBuffer stringBuffer = StringBuffer();
     if (num < 0) {
-      stringBuffer.write(NumberToWordsChineseConstant.negative);
+      stringBuffer.write(_negative);
       num = num.abs();
     }
     stringBuffer.write(_intToChinese(num.toInt()));
-    stringBuffer.write(_doubleToChinese(num.toDouble()));
+    stringBuffer.write(_doubleToChinese(num));
     return stringBuffer.toString();
   }
 
@@ -25,177 +25,170 @@ class NumberToWordsChinese {
     int n = num.abs();
 
     if (n == 0) {
-      return NumberToWordsChineseConstant.zero;
+      return _zero;
     }
 
-    List<String> unitsListCopy = NumberToWordsChineseConstant.unitList.toList();
+    List<String> unitsListCopy = _unitList.toList();
     while (n > 0) {
       final unit = unitsListCopy.removeAt(0);
       final int digit = n % 10;
-      str = NumberToWordsChineseConstant.digitList[digit] + unit + str;
+      str = _digitList[digit] + unit + str;
       n = n ~/ 10;
     }
 
-    const smallUnit = NumberToWordsChineseConstant.ten +
-        NumberToWordsChineseConstant.hundred +
-        NumberToWordsChineseConstant.thousand;
-    const bigUnit = NumberToWordsChineseConstant.tenThousand +
-        NumberToWordsChineseConstant.hundredMillion +
-        NumberToWordsChineseConstant.trillion +
-        NumberToWordsChineseConstant.tenQuadrillion +
-        NumberToWordsChineseConstant.hundredQuintillion;
-    const zero = NumberToWordsChineseConstant.zero;
+    const smallUnit = _ten + _hundred + _thousand;
+    const bigUnit = _tenThousand +
+        _hundredMillion +
+        _trillion +
+        _tenQuadrillion +
+        _hundredQuintillion;
 
     str = str.replaceAllMapped(
-      RegExp("($zero)[$smallUnit]"),
+      RegExp("($_zero)[$smallUnit]"),
       (match) {
         return match.group(1)!;
       },
     ).replaceAllMapped(
       RegExp("([$bigUnit])[^$smallUnit]+([$bigUnit])"),
       (match) {
-        return match.group(1)! + zero;
+        return match.group(1)! + _zero;
       },
     ).replaceAllMapped(
-      RegExp("([$smallUnit])$zero+([$bigUnit])"),
+      RegExp("([$smallUnit])$_zero+([$bigUnit])"),
       (match) {
-        return match.group(1)! + match.group(2)! + zero;
+        return match.group(1)! + match.group(2)! + _zero;
       },
     ).replaceAllMapped(
-      RegExp("($zero)+"),
+      RegExp("($_zero)+"),
       (match) {
         return match.group(1)!;
       },
     ).replaceAllMapped(
-      RegExp(zero + r"+$"),
+      RegExp(_zero + r"+$"),
       (match) {
         return '';
       },
     ).replaceAllMapped(
-      RegExp(
-          "^${NumberToWordsChineseConstant.one}${NumberToWordsChineseConstant.ten}"),
+      RegExp("^$_one$_ten"),
       (match) {
-        return NumberToWordsChineseConstant.ten;
+        return _ten;
       },
     );
     return str;
   }
 
   /// Covert decimal number to chinese words.
-  static String _doubleToChinese(double num) {
+  static String _doubleToChinese(num num) {
     if (num % 1 == 0) return '';
 
     String str = '';
     final String n = num.abs().toString().split('.').last;
     final List<String> nSplit = n.split('');
     for (String i in nSplit) {
-      str += NumberToWordsChineseConstant.digitList[int.parse(i)];
+      str += _digitList[int.parse(i)];
     }
 
-    return NumberToWordsChineseConstant.dot + str;
+    return _dot + str;
   }
 }
 
 /// Chinese number to words constant
-class NumberToWordsChineseConstant {
-  NumberToWordsChineseConstant._();
 
-  /// Chinese zero (0)
-  static const String zero = '零';
+/// Chinese zero (0)
+const String _zero = '零';
 
-  /// Chinese one (1)
-  static const String one = '一';
+/// Chinese one (1)
+const String _one = '一';
 
-  /// Chinese two (2)
-  static const String two = '二';
+/// Chinese two (2)
+const String _two = '二';
 
-  /// Chinese three (3)
-  static const String three = '三';
+/// Chinese three (3)
+const String _three = '三';
 
-  /// Chinese four (4)
-  static const String four = '四';
+/// Chinese four (4)
+const String _four = '四';
 
-  /// Chinese five (5)
-  static const String five = '五';
+/// Chinese five (5)
+const String _five = '五';
 
-  /// Chinese six (6)
-  static const String six = '六';
+/// Chinese six (6)
+const String _six = '六';
 
-  /// Chinese seven (7)
-  static const String seven = '七';
+/// Chinese seven (7)
+const String _seven = '七';
 
-  /// Chinese eight (8)
-  static const String eight = '八';
+/// Chinese eight (8)
+const String _eight = '八';
 
-  /// Chinese nine (9)
-  static const String nine = '九';
+/// Chinese nine (9)
+const String _nine = '九';
 
-  /// Chinese digit list from 0 - 9
-  static const List<String> digitList = [
-    zero,
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-  ];
+/// Chinese digit list from 0 - 9
+const List<String> _digitList = [
+  _zero,
+  _one,
+  _two,
+  _three,
+  _four,
+  _five,
+  _six,
+  _seven,
+  _eight,
+  _nine,
+];
 
-  /// Chinese ten (10)
-  static const String ten = '十';
+/// Chinese ten (10)
+const String _ten = '十';
 
-  /// Chinese hundred (100)
-  static const String hundred = '百';
+/// Chinese hundred (100)
+const String _hundred = '百';
 
-  /// Chinese thousand (1000)
-  static const String thousand = '千';
+/// Chinese thousand (1000)
+const String _thousand = '千';
 
-  /// Chinese ten thousand (10000)
-  static const String tenThousand = '萬';
+/// Chinese ten thousand (10000)
+const String _tenThousand = '萬';
 
-  /// Chinese hundred million (100000000)
-  static const String hundredMillion = '億';
+/// Chinese hundred million (100000000)
+const String _hundredMillion = '億';
 
-  /// Chinese trillion (1000000000000)
-  static const String trillion = '兆';
+/// Chinese trillion (1000000000000)
+const String _trillion = '兆';
 
-  /// Chinese ten quadrillion (10000000000000000)
-  static const String tenQuadrillion = '京';
+/// Chinese ten quadrillion (10000000000000000)
+const String _tenQuadrillion = '京';
 
-  /// Chinese hundred quintillion (100000000000000000000)
-  static const String hundredQuintillion = '垓';
+/// Chinese hundred quintillion (100000000000000000000)
+const String _hundredQuintillion = '垓';
 
-  /// Chinese unit list
-  static const List<String> unitList = [
-    '',
-    ten,
-    hundred,
-    thousand,
-    tenThousand,
-    ten,
-    hundred,
-    thousand,
-    hundredMillion,
-    ten,
-    hundred,
-    thousand,
-    trillion,
-    ten,
-    hundred,
-    thousand,
-    tenQuadrillion,
-    ten,
-    hundred,
-    thousand,
-    hundredQuintillion,
-  ];
+/// Chinese unit list
+const List<String> _unitList = [
+  '',
+  _ten,
+  _hundred,
+  _thousand,
+  _tenThousand,
+  _ten,
+  _hundred,
+  _thousand,
+  _hundredMillion,
+  _ten,
+  _hundred,
+  _thousand,
+  _trillion,
+  _ten,
+  _hundred,
+  _thousand,
+  _tenQuadrillion,
+  _ten,
+  _hundred,
+  _thousand,
+  _hundredQuintillion,
+];
 
-  /// Chinese negative (-)
-  static const String negative = '負';
+/// Chinese negative (-)
+const String _negative = '負';
 
-  /// Chinese dot (.)
-  static const String dot = '點';
-}
+/// Chinese dot (.)
+const String _dot = '點';
